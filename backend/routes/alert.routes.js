284ba9692465a0b2fd6131dbc3afba94524de41c
KunnuSherry <gamingvenom465@gmail.com>
2025-06-18 17:05:43 +0530

    c6:- Test Route

--- backend/routes/alert.routes.js
+++ backend/routes/alert.routes.js
@@ -4,5 +4,9 @@ import { receiveAlert } from "../contollers/alert.controller.js";
 const router = express.Router();
 
 router.post("/alert", receiveAlert);
+router.get("/ping", (req, res) => {
+  res.send("Alert router is working ✅");
+});
+
 
 export default router;
